58c8f499f5fcfd254ce453cda1f44842621aa092
chamoyo <chamoyosoy@hotmail.com>
2025-07-13 01:20:35 -0600

    import genai dependencies

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from ai-agent!")
+import sys
+import os
+from dotenv import load_dotenv
+from google import genai
 
+def main():
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents='Why is the sky blue?'
+    )
+    print(response.text)
+    metadata = response.usage_metadata
+    print(f"Prompt tokens: {metadata.prompt_token_count}")
+    print(f"Response tokens: {metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
