3ce9bac4cfc5f65085f056852682e086506f689a
chamoyo <chamoyosoy@hotmail.com>
2025-07-13 01:21:31 -0600

    add user input

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ from google import genai
 def main():
     load_dotenv()
 
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is the sky blue?'
+    model='gemini-2.0-flash-001', contents=user_prompt
     )
     print(response.text)
     metadata = response.usage_metadata
@@ -19,3 +28,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
