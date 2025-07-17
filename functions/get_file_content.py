df3cb3c9c126a85ea44a41c8b4a1ec84eba020fe
chamoyo <chamoyosoy@hotmail.com>
2025-07-17 06:20:02 -0600

    modified get_file_content.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -3,13 +3,10 @@ from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
-    target_dir = abs_working_dir
-
-    # if have directory join the paths togheter
-    if file_path:
-        target_dir = os.path.abspath(os.path.join(working_directory, file_path))
+    abs_file_path = target_dir = os.path.abspath(os.path.join(working_directory, file_path))
+        
     # validate it stays within the working directory boundaries
-    if not target_dir.startswith(abs_working_dir):
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
     # validate is it a file
     if not os.path.isfile(target_dir):
@@ -18,9 +15,9 @@ def get_file_content(working_directory, file_path):
     try:
         with open(target_dir, "r") as f:
             file_content = f.read(MAX_CHARS)
-            if not len(file_content) <= MAX_CHARS:
-                return file_content + f'...File "{file_path}" truncated at 1000 characters'
-            return file_content
+            if os.path.getsize(abs_file_path):
+                file_content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return file_content
         
     except Exception as e:
         return f"Error accesing file: {e}"
