94bc76e65cc061ba797c488f1edd5c6131f2e805
chamoyo <chamoyosoy@hotmail.com>
2025-07-19 00:33:51 -0600

    modified get_file_content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -5,18 +5,18 @@ def get_file_content(working_directory, file_path):
     # get absolute path form a relative path
     abs_working_dir = os.path.abspath(working_directory)
     # join the paths togheter
-    abs_file_path = target_dir = os.path.abspath(os.path.join(working_directory, file_path))
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
         
     # validate it stays within the working directory boundaries
     if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
     # validate is it a file
-    if not os.path.isfile(target_dir):
+    if not os.path.isfile(abs_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
     
     try:
         # open the file as a file
-        with open(target_dir, "r") as f:
+        with open(abs_file_path, "r") as f:
             # save with a limit of chars
             file_content = f.read(MAX_CHARS)
             # if the file has more than the limit add a str indicated at the end
