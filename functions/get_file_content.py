2546456f61ca70fc041d9e2158024b2d0c68df4b
chamoyo <chamoyosoy@hotmail.com>
2025-07-20 22:46:34 -0600

    add all funtions to calling options

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,6 @@
 import os
 from config import MAX_CHARS
+from google.genai import types
 
 def get_file_content(working_directory, file_path):
     # get absolute path form a relative path
@@ -27,3 +28,18 @@ def get_file_content(working_directory, file_path):
     # if something goes wrong return a error
     except Exception as e:
         return f"Error accesing file: {e}"
+
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+    ),
+)
