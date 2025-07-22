2aa078b6dbfec247c9c672aa3643870d3900f54f
chamoyo <chamoyosoy@hotmail.com>
2025-07-21 22:20:30 -0600

    add feedback loop

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+
 from prompts import system_prompt
 from call_function import available_functions, call_function
+from config import MAX_ITERS
 
 def main():
     # loads environment variables from a .env
@@ -25,34 +27,56 @@ def main():
     # join the prompt in a sigle str cause normally is saved as a list of words(strs)
     user_prompt = " ".join(args)
 
-    # save the apikey and the chosed client to use
+    # select the apikey and then chosed client to use
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    # if --verbose is passed print the user prompt a the begining
     if verbose:
         print(f"User prompt: {user_prompt}")
 
+    # pass the prompt as the client need 
     prompt=[
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-    generate(client, prompt, verbose)
+    # call generate funtion which is the function to generate content and call funcs
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f'Maximum iterations ({MAX_ITERS}) reached')
+            sys.exit(1)
+        try:
+            final_response = generate(client, prompt, verbose)
+            if final_response:
+                print("final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f'Error in generate content: {e}')
 
 
 def generate(client, prompt, verbose):
-
+    # generate the response with some alligment
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents=prompt,
-    config=types.GenerateContentConfig(
+    model='gemini-2.0-flash-001', #select th model who use
+    contents=prompt, # pass the menssge from the user
+    config=types.GenerateContentConfig( # pass the available functions to use and a firts instruction prompt, saved in "prompts.py"
     tools=[available_functions], system_instruction=system_prompt
         ),
     )
 
-    # if not function is calling return only a response
+    if response.candidates:
+        for candidate in response.candidates:
+            function_call_content = candidate.content
+            prompt.append(function_call_content)
+
+    # if not functions are called return only a response
     if not response.function_calls:
         return response.text
 
+    # when func is needed call it and save in a list 
     function_responses = []
     for function_call_part in response.function_calls:
         function_call_result = call_function(function_call_part, verbose)
@@ -65,9 +89,12 @@ def generate(client, prompt, verbose):
             print(f"-> {function_call_result.parts[0].function_response.response}")
         function_responses.append(function_call_result.parts[0])
 
+
     if not function_responses:
         raise Exception("no function responses generated, exiting.")
 
+    prompt.append(types.Content(role="tool", parts=function_responses))
+    # if -- verbose was pass print metadata from response
     if verbose:
         metadata = response.usage_metadata
         print(f"Prompt tokens: {metadata.prompt_token_count}")
